4c78e0da54d6c7011931e22a2d0c525582a43752
DjArt <art.letter.mail@gmail.com>
2019-11-07 20:05:45 +0700

    Добавлены интеракторы

---
+++ Tractor.Core/Interactors/AccountManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Model;
+using Tractor.Core.Objects;
+
+namespace Tractor.Core.Interactors
+{
+    public sealed class AccountManager
+    {
+        public List<Account> Accounts { get; }
+
+        public void UnloadAccount(Account account)
+        {
+
+        }
+
+        public void LoadAccount(Account account)
+        {
+
+        }
+
+
+    }
+}
---
+++ Tractor.Core/Interactors/DBManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Model;
+using Tractor.Core.Objects;
+
+namespace Tractor.Core.Interactors
+{
+    public sealed class DBManager
+    {
+        public Dictionary<Account, DataBase> DataBases { get; }
+        
+        public void CreateDataBase(Account account)
+        {
+
+        }
+
+        public void LoadDataBase(Account account)
+        {
+
+        }
+
+        private void SyncDataBase(Account account)
+        {
+
+        }
+
+        public void UnloadDataBase(Account account)
+        {
+
+        }
+    }
+}
