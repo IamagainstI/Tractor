8b299f1fd3f8e0ce78b28941b0414bea10becb22
herhartman <chopktv@gmail.com>
2019-11-20 02:37:50 +0700

    Добавил интерфейс "IPermission" Добавил интерфейс "IEntityRolePermission" Добавил интерфейс "IEntityPermission"

---
+++ Tractor.Core/Objects/Entities/Permissions/IEntityPermission.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Objects.Entities.Permissions
+{
+    interface IEntityPermission : IPermission
+    {
+    }
+}
