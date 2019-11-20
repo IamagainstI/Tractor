9542f0cd07871c9d427749dd5e2dc3d6badd8999
herhartman <chopktv@gmail.com>
2019-11-20 22:57:28 +0700

    GoogleDriveStorageRepository added

---
+++ Tractor.Core/Objects/Repositories/GoogleDriveStorageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects.Credentials;
+
+namespace Tractor.Core.Objects.Repositories
+{
+    class GoogleDriveStorageRepository : ICloudStarageRepository
+    {
+        public ICredentials Credentials => throw new NotImplementedException();
+    }
+}
