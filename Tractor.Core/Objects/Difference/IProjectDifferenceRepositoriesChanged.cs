759f402def86a25a002c49bd337668a444f3b9e8
herhartman <chopktv@gmail.com>
2019-11-20 21:35:42 +0700

    IProjectDifferenceRepositoriesChanged added

---
+++ Tractor.Core/Objects/Difference/IProjectDifferenceRepositoriesChanged.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Objects.Difference
+{
+    public interface IProjectDifferenceRepositoriesChanged : IProjectDifference
+    {
+        IList<IRepositoryDifference> Repositories { get; }
+    }
+}
