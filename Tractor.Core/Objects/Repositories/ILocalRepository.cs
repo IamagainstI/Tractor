2fdcfc3986926bf7bccc3f458fbca1750d836b0f
herhartman <chopktv@gmail.com>
2019-11-20 20:43:01 +0700

    ILocalRepository added

---
+++ Tractor.Core/Objects/Repositories/ILocalRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Objects.Repositories
+{
+    public interface ILocalRepository : IRepository
+    {
+        string Path { get; }
+    }
+}
