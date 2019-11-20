7292af5fd2d6c129ffd1d89e25387064ee1b3ae2
herhartman <chopktv@gmail.com>
2019-11-20 20:11:23 +0700

    Добавил интерфейс "IEventTask"

---
+++ Tractor.Core/Objects/Tasks/IEventTask.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Objects.Tasks
+{
+    interface IEventTask : ITask
+    {
+        TimeSpan Duration { get; }
+        DateTime StartTime { get; }
+    }
+}
