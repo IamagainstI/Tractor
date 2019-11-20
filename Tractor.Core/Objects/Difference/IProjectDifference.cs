529cb98d4bb451ba2c6bf7750ff4668d3d820f27
DjArt <art.letter.mail@gmail.com>
2019-11-20 20:21:54 +0700

    IProjectDifference added

---
+++ Tractor.Core/Objects/Difference/IProjectDifference.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects.Projects;
+
+namespace Tractor.Core.Objects.Difference
+{
+    public interface IProjectDifference
+    {
+        IProject Project { get; }
+    }
+}
