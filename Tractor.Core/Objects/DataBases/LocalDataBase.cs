db04349b7ff5c696432b33441311cd9582fe214d
DjArt <art.letter.mail@gmail.com>
2019-11-20 21:25:09 +0700

    fixed namespace & properties

--- Tractor.Core/Objects/DataBases/LocalDataBase.cs
+++ Tractor.Core/Objects/DataBases/LocalDataBase.cs
@@ -4,22 +4,18 @@ using System.Text;
 using Tractor.Core.Model;
 using Tractor.Core.Objects.Difference;
 using Tractor.Core.Objects.Entities.Permissions;
+using Tractor.Core.Objects.Projects;
 using Tractor.Core.Objects.Repositories;
 
 namespace Tractor.Core.Objects.DataBases
 {
     public class LocalDataBase : IDataBase
     {
-        public TractorAccount Account => throw new NotImplementedException();
-
-        public IEnumerable<IEntity> Entities => throw new NotImplementedException();
-
-        public IEnumerable<IProject> Projects => throw new NotImplementedException();
-
-        public IEnumerable<IPermission> Permissions => throw new NotImplementedException();
-
-        public IEnumerable<IRepository> Repositories => throw new NotImplementedException();
-
-        public IEnumerable<IDifference> History => throw new NotImplementedException();
+        public TractorAccount Account { get; set; }
+        public IEnumerable<IEntity> Entities { get; }
+        public IEnumerable<IProject> Projects { get; }
+        public IEnumerable<IPermission> Permissions { get; }
+        public IEnumerable<IRepository> Repositories { get; }
+        public IEnumerable<IDifference> History { get; }
     }
 }
