31d9136aa745392dd85dc72bf20d9bbd9119f68e
herhartman <chopktv@gmail.com>
2019-11-20 20:42:45 +0700

    INetworkRepository added

---
+++ Tractor.Core/Objects/Repositories/INetworkRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects.Credentials;
+
+namespace Tractor.Core.Objects.Repositories
+{
+    public interface INetworkRepository : IRepository
+    {
+        ICredentials Credentials { get; }
+    }
+}
