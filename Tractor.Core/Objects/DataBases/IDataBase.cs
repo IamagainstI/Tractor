38f975ee665e476147e69963b914eaa6756cf72e
DjArt <art.letter.mail@gmail.com>
2019-11-20 19:28:28 +0700

    Доавлен и реализован IDataBase

---
+++ Tractor.Core/Objects/DataBases/IDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects.Difference;
+using Tractor.Core.Objects.Entities.Permissions;
+using Tractor.Core.Objects.Repositories;
+
+namespace Tractor.Core.Objects.DataBases
+{
+    public interface IDataBase
+    {
+        TractorAccount Account { get; }
+        IEnumerable<IEntity> Entities { get; }
+        IEnumerable<IProject> Projects { get; }
+        IEnumerable<IPermission> Permissions { get; }
+        IEnumerable<IRepository> Repositories { get; }
+        IEnumerable<IDifference> History { get; }
+    }
+}
---
+++ Tractor.Core/Objects/DataBases/LocalDataBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Model;
+using Tractor.Core.Objects.Difference;
+using Tractor.Core.Objects.Entities.Permissions;
+using Tractor.Core.Objects.Repositories;
+
+namespace Tractor.Core.Objects.DataBases
+{
+    public class LocalDataBase : IDataBase
+    {
+        public TractorAccount Account => throw new NotImplementedException();
+
+        public IEnumerable<IEntity> Entities => throw new NotImplementedException();
+
+        public IEnumerable<IProject> Projects => throw new NotImplementedException();
+
+        public IEnumerable<IPermission> Permissions => throw new NotImplementedException();
+
+        public IEnumerable<IRepository> Repositories => throw new NotImplementedException();
+
+        public IEnumerable<IDifference> History => throw new NotImplementedException();
+    }
+}
