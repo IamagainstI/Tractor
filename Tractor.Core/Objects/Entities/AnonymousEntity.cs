f518c11920de96d836ccdbcd456b89d0f33df4fe
herhartman <chopktv@gmail.com>
2019-11-20 21:33:19 +0700

    AnonymousEntity added

---
+++ Tractor.Core/Objects/Entities/AnonymousEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Objects.Entities
+{
+    class AnonymousEntity : IEntity
+    {
+        public string Name => throw new NotImplementedException();
+
+        public Guid ID => throw new NotImplementedException();
+    }
+}
