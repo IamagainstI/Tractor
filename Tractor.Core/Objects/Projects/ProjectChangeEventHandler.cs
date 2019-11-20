1ea0de0f81195dedb93e178154d4cdf3bf4dc3fe
DjArt <art.letter.mail@gmail.com>
2019-11-20 20:20:17 +0700

    ProjectChangedEventHandler added

---
+++ Tractor.Core/Objects/Projects/ProjectChangeEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Objects.Projects
+{
+    public delegate void ProjectChangeEventHandler(IProjectDifference difference)
+}
