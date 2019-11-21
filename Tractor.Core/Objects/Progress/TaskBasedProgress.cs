dfde1e6b138cf4cab3733cc0d600b26bf5621c1a
DjArt <art.letter.mail@gmail.com>
2019-11-21 21:05:26 +0700

    TaskBased progress partially fixed

--- Tractor.Core/Objects/Progress/TaskBasedProgress.cs
+++ Tractor.Core/Objects/Progress/TaskBasedProgress.cs
@@ -1,14 +1,31 @@
-﻿using System;
+﻿using EmptyBox.Collections.Generic;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Tractor.Core.Objects.Tasks;
 
 namespace Tractor.Core.Objects.Progress
 {
     public class TaskBasedProgress : IProgress
     {
-        public Progress Percentage => throw new NotImplementedException();
+        public Progress Percentage
+        {
+            get
+            {
+                foreach (ITask task in Tasks)
+                {
+                    task.Progress.Percentage
+                }
+            }
+        }
 
         public DateTime TimeLastChangeProgress => throw new NotImplementedException();
+        public ITreeNode<ITask> Tasks { get; }
+
+        public TaskBasedProgress(ITreeNode<ITask> tasks)
+        {
+            Tasks = tasks;
+        }
 
         public bool Equals(IProgress other)
         {
