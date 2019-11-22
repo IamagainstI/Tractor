0c49f174022647c0ddc753ae9c3356086e41d567
herhartman <chopktv@gmail.com>
2019-11-23 02:54:12 +0700

    DescriptionChangeEventHandler added

---
+++ Tractor.Core/Objects/Description/DesctiptionChangeEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects.Difference;
+
+namespace Tractor.Core.Objects.Description
+{
+    public delegate void DescriptionChangeEventHandler(IDescritptionDifference difference);
+}
