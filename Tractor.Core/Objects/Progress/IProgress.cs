e9e62177736bca7f91c6dbb6dfd8f4e10a71ed2e
sivtsev.107911 <sivtsev.107911@gmail.com>
2019-11-25 21:11:51 +0700

    опиcание интерфейcа IProgress

--- Tractor.Core/Objects/Progress/IProgress.cs
+++ Tractor.Core/Objects/Progress/IProgress.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using Tractor.Core.Objects.Difference;
 
 namespace Tractor.Core.Objects.Progress
 {
-    public interface IProgress : IEquatable<IProgress>
+    public interface IProgress : IEquatable<IProgress>, INotifyPropertyChanged
     {
-        double Percentage { get; }
-        void ProgressChanged(IProgress Difference);
+        double ProgressPercentage { get; }
         DateTime TimeLastChangeProgress { get; }
     }
 }
