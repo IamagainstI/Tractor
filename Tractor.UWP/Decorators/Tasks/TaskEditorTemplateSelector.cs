ed397b54d6719db2120ef3f08271b966b4082c72
DjArt <art.letter.mail@gmail.com>
2019-12-01 01:53:06 +0700

    TaskEditorTemplateSelector добавлен

---
+++ Tractor.UWP/Decorators/Tasks/TaskEditorTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Tractor.UWP.Decorators.Tasks
+{
+    public sealed class TaskEditorTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
