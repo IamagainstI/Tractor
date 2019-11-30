9cf36e594f9a4af0ebdfd2af770e21bd8d828f79
DjArt <art.letter.mail@gmail.com>
2019-12-01 01:33:34 +0700

    Добавлен пул констант-имён страниц

---
+++ Tractor.Core/Routers/UI/UIViews.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Routers.UI
+{
+    public static class UIViews
+    {
+        public const string OVERALL_PAGE = nameof(OVERALL_PAGE);
+        public const string PROJECT_MANAGEMENT_PAGE = nameof(PROJECT_MANAGEMENT_PAGE);
+        public const string PROJECTS_PAGE = nameof(PROJECTS_PAGE);
+        public const string TEAMS_MANAGEMENT_PAGE = nameof(TEAMS_MANAGEMENT_PAGE);
+        public const string USER_SETUP_PAGE = nameof(USER_SETUP_PAGE);
+    }
+}
