656e5bf7ae8cf056f0703593d5531dac084f5920
DjArt <art.letter.mail@gmail.com>
2019-12-01 17:43:11 +0700

    IEnumerable и IReadOnlyDictionary были заменены на ObservableDictionary и ObservableCollection несоответсвтвенно

--- Tractor.Core/Objects/Tasks/ITask.cs
+++ Tractor.Core/Objects/Tasks/ITask.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Text;
+using Tractor.Core.Collections;
 using Tractor.Core.Model;
 using Tractor.Core.Objects.Descriptions;
 using Tractor.Core.Objects.Progress;
@@ -17,26 +18,14 @@ namespace Tractor.Core.Objects.Tasks
     {
         string Name { get; set; }
         IDescription Description { get; set; }
-        IEnumerable<ITask> Subtasks { get; }
-        IEnumerable<ITask> Dependencies { get; }
-        IEnumerable<IEntity> Observers { get; }
+        ObservableCollection<ITask> Subtasks { get; }
+        ObservableCollection<ITask> Dependencies { get; }
+        ObservableCollection<IEntity> Observers { get; }
         IEntity Performer { get; set; }
         IEntity Producer { get; set; }
         IProgress Progress { get; set; }
         DateTime CreationDate { get; }
         DateTime LastStateChangeDate { get; }
         ITaskLocation Location { get; set; }
-        void AddSubtask(ITask subtask);
-        void AddObserver(IEntity observer);
-        void AddDependency(ITask dependency);
-        void RemoveSubtask(ITask SubTask);
-        void RemoveObserver(IEntity observer);
-        void RemoveDependency(ITask dependency);
-        void AddRangeSubtask(IEnumerable<ITask> subtasks);
-        void AddRangeObserver(IEnumerable<IEntity> observers);
-        void AddRangeDependency(IEnumerable<ITask> dependencies);
-        void RemoveRangeSubtask(IEnumerable<ITask> subtasks);
-        void RemoveRangeObserver(IEnumerable<IEntity> observers);
-        void RemoveRangeDependency(IEnumerable<ITask> dependencies);
     }
 }
