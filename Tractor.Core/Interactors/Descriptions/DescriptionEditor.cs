3b58e54880ff3de0eb36739d739b0275daef9c4b
Гартман Артем <chopktv@gmail.com>
2019-12-01 19:03:05 +0700

    Реализовал DescriptionEditor

--- Tractor.Core/Interactors/Descriptions/DescriptionEditor.cs
+++ Tractor.Core/Interactors/Descriptions/DescriptionEditor.cs
@@ -1,10 +1,13 @@
 ﻿using EmptyBox.Automation;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using Tractor.Core.Objects;
+using Tractor.Core.Objects.Descriptions;
 using Tractor.Core.Routers.UI;
+using Tractor.Core.Specialized;
 
 namespace Tractor.Core.Interactors.Descriptions
 {
@@ -23,6 +26,7 @@ namespace Tractor.Core.Interactors.Descriptions
         }
 
         public IDescription Description { get; set; }
+        public Type Type { get => Type; set => TypeChange(value); }
 
         public void EdnEditing()
         {
@@ -37,12 +41,38 @@ namespace Tractor.Core.Interactors.Descriptions
             NavigationInfo_Output?.Invoke(this, new NavigationInfo() { Name = "Back" });
         }
 
+        private void TypeChange(Type type)
+        {
+            if (StoredDescription.GetType() != type)
+            {
+                if (typeof(IDescription).IsAssignableFrom(type))
+                {
+                    ConstructorInfo constructorInfo = type.GetConstructor(new[] { typeof(Guid) });
+                    object project = constructorInfo?.Invoke(new object[] { StoredDescription.ID });
+                    TypeInfo newTypeInfo = type.GetTypeInfo();
+                    TypeInfo oldTypeInfo = StoredDescription.GetType().GetTypeInfo();
+                    IEnumerable<PropertyInfo> intersect = newTypeInfo.DeclaredProperties.Intersect(oldTypeInfo.DeclaredProperties, PropertyComparator.Comparator);
+                    foreach (PropertyInfo prop in intersect)
+                    {
+                        if (prop.CanWrite)
+                        {
+                            PropertyInfo readProp = oldTypeInfo.GetDeclaredProperty(prop.Name);
+                            prop.SetValue(project, readProp.GetValue(Description));
+                        }
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Some exception");
+                }
+            }
+        }
+
         private void GetData(object sender, IDescription data)
         {
             StoredDescription = data;
             Description = (IDescription)data.Clone();
             NavigationInfo_Output?.Invoke(this, new NavigationInfo() { Name = "DescriptionEditor" });
         }
-
     }
 }
