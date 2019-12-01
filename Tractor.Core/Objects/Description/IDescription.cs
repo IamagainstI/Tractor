ce3a84988863fb13417355e40055dfee3718aafa
Гартман Артем <chopktv@gmail.com>
2019-12-01 15:58:10 +0700

    IDescription Clone() realisation

--- Tractor.Core/Objects/Description/IDescription.cs
+++ Tractor.Core/Objects/Description/IDescription.cs
@@ -5,9 +5,10 @@ using EmptyBox.IO.Storage;
 
 namespace Tractor.Core.Objects
 {
-    public interface IDescription : IEquatable<IDescription>, INotifyPropertyChanged, INotifyPropertyChanging
+    public interface IDescription : IEquatable<IDescription>, INotifyPropertyChanged, INotifyPropertyChanging, ICloneable
     {
 		IList<ILabel> Labels { get; }
+        Guid ID { get; }
         IList<IStorageItem> Attachments { get; }
         void AddLabel(IEnumerable<ILabel> label);
         void RemoveLabel(IEnumerable<ILabel> label);
