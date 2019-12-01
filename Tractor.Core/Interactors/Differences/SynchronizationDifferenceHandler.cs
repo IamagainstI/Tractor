c2539d455d91eaed9592cb208645a32c520c5c06
DjArt <art.letter.mail@gmail.com>
2019-12-01 20:22:29 +0700

    Добавлен SynchronizationDifferenceHandler

---
+++ Tractor.Core/Interactors/Differences/SynchronizationDifferenceHandler.cs
@@ -0,0 +1,19 @@
+﻿using EmptyBox.Automation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects.Difference;
+
+namespace Tractor.Core.Interactors.Differences
+{
+    public class SynchronizationDifferenceHandler : IPipelineOutput<IDifference>
+    {
+        private event EventHandler<IDifference> IDifference_Output;
+
+        event EventHandler<IDifference> IPipelineOutput<IDifference>.Output
+        {
+            add => IDifference_Output += value;
+            remove => IDifference_Output -= value;
+        }
+    }
+}
