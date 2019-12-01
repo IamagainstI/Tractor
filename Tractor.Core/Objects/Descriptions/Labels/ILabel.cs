ffaf4654147bcec06b44b4e9f0e0d8c71f7b8d51
Гартман Артем <chopktv@gmail.com>
2019-12-01 19:04:43 +0700

    Добавил конструктор + клонирование в Label

--- Tractor.Core/Objects/Descriptions/Labels/ILabel.cs
+++ Tractor.Core/Objects/Descriptions/Labels/ILabel.cs
@@ -5,9 +5,10 @@ using Tractor.Core.Model;
 
 namespace Tractor.Core.Objects.Descriptions.Labels
 {
-    public interface ILabel : IEquatable<ILabel>, INotifyPropertyChanged, INotifyPropertyChanging
+    public interface ILabel : IEquatable<ILabel>, INotifyPropertyChanged, INotifyPropertyChanging, ICloneable
     {
         Color Color { get; set; }
         string Name { get; set; }
+        Guid ID { get; }
     }
 }
