52dd996834ab67cc35d0fbc03322eb8bb66e37a6
DjArt <art.letter.mail@gmail.com>
2019-12-03 14:02:29 +0700

    Набросок конструктора конвеера

--- Tractor.Core/Routers/Pipeline/PipelineConstructor.cs
+++ Tractor.Core/Routers/Pipeline/PipelineConstructor.cs
@@ -5,18 +5,29 @@ using Tractor.Core.Interactors.Tasks;
 using Tractor.Core.Routers.UI;
 using EmptyBox.Automation;
 using Tractor.Core.Objects.Tasks;
+using Tractor.Core.Interactors.Differences;
+using Tractor.Core.Interactors.DataBases;
 
 namespace Tractor.Core.Routers.Pipeline
 {
     public class PipelineConstructor
     {
+        public DifferenceMerger DifferenceMerger = new DifferenceMerger();
+        public DifferenceDispenser DifferenceDispenser = new DifferenceDispenser();
+        public RuntimeDifferenceHandler RuntimeDifferenceHandler = new RuntimeDifferenceHandler();
+        public SynchronizationDifferenceHandler SynchronizationDifferenceHandler = new SynchronizationDifferenceHandler();
+        public DataBaseDifferenceHandler DataBaseDifferenceHandler = new DataBaseDifferenceHandler(null);
         public TaskEditor TaskEditor = new TaskEditor();
         public UIRouter Navigator = new UIRouter();
-        public ExternalInput<ITask> ExternalTaskInput = new ExternalInput<ITask>();
 
         public PipelineConstructor()
         {
-            _ = ExternalTaskInput >> TaskEditor;
+            //При изменении элементов во время редактирования отправляем их в DataBaseDifferenceHandler
+            _ = RuntimeDifferenceHandler >> DataBaseDifferenceHandler;
+            //При получении элементов при синхронизации отпарвляем их в DataBaseDifferenceHandler
+            //При получении IDifference от SynchronizationDifferenceHandler обрабатываем её и отправляем в DifferenceDispenser
+            _ = SynchronizationDifferenceHandler >> DataBaseDifferenceHandler >> DifferenceDispenser;
+            //При получении задачи для редактирования открываем соответствующий интерфейс
             _ = TaskEditor >> Navigator;
         }
     }
