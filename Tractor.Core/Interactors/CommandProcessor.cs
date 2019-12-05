87b4354f4f82534f01e4ff8cd3505232615c633e
sivtsev.107911 <sivtsev.107911@gmail.com>
2019-12-05 23:11:56 +0700

    Gartmans interactors

---
+++ Tractor.Core/Interactors/CommandProcessor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EmptyBox.Automation;
+using Tractor.Core.Routers.Command;
+using Tractor.Core.Objects.Difference;
+
+namespace Tractor.Core.Interactors
+{
+    public class CommandProcessor : Pipeline<ICommand, ICommand>, IPipelineIO<ICommand, IDifference>
+    {
+        private event EventHandler<IDifference> IDifference_Output;
+        //private event EventHandler<object> Object_Output;
+
+        event EventHandler<IDifference> IPipelineOutput<IDifference>.Output
+        {
+            add => IDifference_Output += value;
+            remove => IDifference_Output -= value;
+        }
+        //event EventHandler<object> IPipelineOutput<object>.Output
+        //{
+        //    add => Object_Output += value;
+        //    remove => Object_Output -= value;
+        //}
+
+        EventHandler<ICommand> IPipelineInput<ICommand>.Input => CommandHandler;
+
+        private void CommandHandler(object sender, ICommand Command)
+        {
+            Type CommandType = Command.GetType();
+            if (Command is SetCommand)
+            {
+                IDifference difference = new Difference(new Guid())
+                {
+                   //NewValue = ()
+                };
+                IDifference_Output?.Invoke(sender, difference);
+            }
+            else if (Command is RelocateCommand)
+            {
+                IDifference difference = new Difference(new Guid())
+                {
+
+                };
+                IDifference_Output?.Invoke(sender, difference);
+            }
+            else if (Command is GetCommand)
+            {
+                IDifference difference = new Difference(new Guid())
+                {
+
+                };
+            }
+               
+        }
+
+    }
+}
