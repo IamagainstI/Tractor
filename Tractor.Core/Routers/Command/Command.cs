64e0418d8302a9a456314ed5ee6d004812603704
sivtsev.107911 <sivtsev.107911@gmail.com>
2019-12-05 18:45:21 +0700

    команды

---
+++ Tractor.Core/Routers/Command/Command.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects;
+using Tractor.Core.Objects.DataBases;
+using Tractor.Core.Objects.Entities.Permissions;
+
+namespace Tractor.Core.Routers.Command
+{
+    class Command
+    {
+        public AccessType accessType { get; set; }
+        public string Commands { get; set; }
+        public IEntity Entity { get; set; }
+        public LocalDataBase LocalDataBase { get; set; }
+
+
+    }
+}
---
+++ Tractor.Core/Routers/Command/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Routers.Command
+{
+    public static class Commands
+    {
+        public const string GET_TASK = nameof(GET_TASK);
+        public const string GET_PROJECT = nameof(GET_PROJECT);
+        public const string ADD_TASK = nameof(GET_TASK);
+        public const string ADD_PROJECT = nameof(ADD_PROJECT);
+        public const string EDITABLE_PROJECT = nameof(EDITABLE_PROJECT);
+        public const string EDITABLE_TASK = nameof(EDITABLE_TASK);
+        public const string CREATE
+    }
+}
