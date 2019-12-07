7023e7491d524507cfa5b8e20dc409d2b2d890c1
DjArt <art.letter.mail@gmail.com>
2019-12-07 18:22:47 +0700

    Добавлен DataRelocationInfo

---
+++ Tractor.Core/Interactors/DataRelocationInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tractor.Core.Interactors
+{
+    public sealed class DataRelocationInfo
+    {
+        public object OldStorage { get; set; }
+        public object NewStorage { get; set; }
+        public object Object { get; set; }
+    }
+}
