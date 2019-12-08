a83689f644c9ff970186fb4c7c356af39c985f73
DjArt <art.letter.mail@gmail.com>
2019-12-08 20:23:55 +0700

    TeamManagmentPresenter added

---
+++ Tractor.Core/Presenters/IEntities/TeamManagmentPresentor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects;
+using Tractor.Core.Routers.UI;
+
+namespace Tractor.Core.Presenters.IEntities
+{
+    public class TeamManagmentPresentor : AbstractPresentor
+    {
+        ITeam Team { get; }
+
+        UIRouter UIRouter { get; }
+
+        public TeamManagmentPresentor(UIRouter router, ITeam team) : base(router)
+        {
+            Team = team;
+        }
+
+        public void AddEntity()
+        {
+            NavigationInfo info = new NavigationInfo()
+            {
+
+            };
+        }
+
+        public void RemoveEntity(IEntity entity)
+        {
+
+        }
+
+    }
+}
