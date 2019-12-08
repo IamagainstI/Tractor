e2e62798f1b854339391e8bba244290d68926f61
DjArt <art.letter.mail@gmail.com>
2019-12-08 20:39:03 +0700

    Сделали для теста кнопку в TaskEditorPage

---
+++ Tractor.UWP/Decorators/Pages/TaskEditorPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Tractor.Core.Presenters.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Tractor.UWP.Decorators.Pages
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class TaskEditorPage : Page
+    {
+        public TaskEditPresenter Presenter { get; private set; }
+
+        public TaskEditorPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Presenter.Save();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Presenter = e.Parameter as TaskEditPresenter;
+            base.OnNavigatedTo(e);
+        }
+    }
+}
