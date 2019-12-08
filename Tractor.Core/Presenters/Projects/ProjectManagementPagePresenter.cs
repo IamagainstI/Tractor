bf13535d83722f2c5afbad9c7406678a1fa43fc7
DjArt <art.letter.mail@gmail.com>
2019-12-08 20:26:11 +0700

    в проект презентор добавил AddSubProject and AddTask

--- Tractor.Core/Presenters/Projects/ProjectManagementPagePresenter.cs
+++ Tractor.Core/Presenters/Projects/ProjectManagementPagePresenter.cs
@@ -3,34 +3,69 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using Tractor.Core.Objects.DataBases;
 using Tractor.Core.Objects.Projects;
 using Tractor.Core.Objects.Tasks;
+using Tractor.Core.Presenters.Tasks;
+using Tractor.Core.Routers.Command;
+using Tractor.Core.Routers.UI;
 
 namespace Tractor.Core.Presenters.Projects
 {
-    public class ProjectManagementPagePresenter : INotifyPropertyChanged
+    public class ProjectManagementPagePresenter : AbstractPresentor, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public IProject PresentedProject { get; }
-        public IEnumerable<ITask> ToDoTasks => PresentedProject.Tasks.Where(x => x.Progress.ProgressPercentage == 0);
-        public IEnumerable<ITask> InProgressTasks => PresentedProject.Tasks.Where(x => x.Progress.ProgressPercentage > 0 && x.Progress.ProgressPercentage < 1);
-        public IEnumerable<ITask> DoneTasks => PresentedProject.Tasks.Where(x => x.Progress.ProgressPercentage == 1);
+        public IProject Project { get; }
+        public IEnumerable<ITask> ToDoTasks => Project.Tasks;
+        public IEnumerable<ITask> InProgressTasks => Project.Tasks.Where(x => x.Progress.ProgressPercentage > 0 && x.Progress.ProgressPercentage < 1);
+        public IEnumerable<ITask> DoneTasks => Project.Tasks.Where(x => x.Progress.ProgressPercentage == 1);
 
-        public ProjectManagementPagePresenter(IProject presented)
+        public ProjectManagementPagePresenter(UIRouter router, IProject presented) : base(router)
         {
-            PresentedProject = presented;
-            PresentedProject.PropertyChanged += PresentedProject_PropertyChanged;
+            Project = presented;
+            Project.PropertyChanged += PresentedProject_PropertyChanged;
         }
 
         private void PresentedProject_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            if (e.PropertyName == nameof(PresentedProject.Tasks))
+            if (e.PropertyName == nameof(Project.Tasks))
             {
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ToDoTasks)));
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(InProgressTasks)));
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DoneTasks)));
             }
         }
+
+        public void AddTask()
+        {
+            NavigationHistory info = new NavigationHistory()
+            {
+                Name = UIViews.TASK_EDITOR,
+                PresenterType = typeof(TaskEditPresenter),
+                Paths = new []
+                {
+                    new List<Guid>(Router.CurrentDataBase.GetPath(Project)),
+                    new List<Guid>() { Guid.NewGuid() }
+                }
+            };
+            Router.RequestNavigation(info);
+        }
+
+        public void AddSubProject()
+        {
+            NavigationHistory history = new NavigationHistory()
+            {
+                Name = UIViews.PROJECT_MANAGEMENT_PAGE,
+                PresenterType = typeof(ProjectManagementPagePresenter),
+                Paths = new[]
+                {
+                    new List<Guid>(Router.CurrentDataBase.GetPath(Project)),
+                    new List<Guid>() { Guid.NewGuid() }
+                }
+            };
+            Router.RequestNavigation(history);
+        }
+
     }
 }
