d58c671ded22e8c18e7db28704075be819345f1a
DjArt <art.letter.mail@gmail.com>
2019-12-08 20:37:08 +0700

    В TractorInstance связали элементы конвейера

--- Tractor.Core/TractorInstance.cs
+++ Tractor.Core/TractorInstance.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Tractor.Core.Interactors;
+using Tractor.Core.Interactors.DataBases;
 using Tractor.Core.Objects;
 using Tractor.Core.Objects.DataBases;
+using Tractor.Core.Objects.Projects;
 using Tractor.Core.Routers.UI;
 
 namespace Tractor.Core
@@ -12,6 +15,11 @@ namespace Tractor.Core
         public TractorAccount CurrentAccount { get; private set; }
         public IDataBase CurrentDataBase { get; set; }
         public UIRouter UIRouter { get; } = new UIRouter();
+        public DataGetter DataGetter { get; } = new DataGetter();
+        public DataBaseDifferenceHandler DataBaseDifferenceHandler { get; }
+        public CommandProcessor CommandProcessor { get; } = new CommandProcessor();
+        public DataEditor DataEditor { get; } = new DataEditor();
+        public DataRelocator DataRelocator { get; } = new DataRelocator();
 
         public TractorInstance()
         {
@@ -20,11 +28,22 @@ namespace Tractor.Core
                 Account = new TractorAccount(Guid.NewGuid()) { Name = "Me" }
             };
             CurrentDataBase = a;
+            UIRouter.CurrentAccount = CurrentAccount;
+            UIRouter.CurrentDataBase = CurrentDataBase;
+            DataBaseDifferenceHandler = new DataBaseDifferenceHandler(CurrentDataBase);
+            //////////////////////
+            a.Projects.Add(new UsualProject(Guid.NewGuid()) { Name = "Хуй", Parent = CurrentDataBase });
+            //////////////////////
+            _ = UIRouter >> CommandProcessor;
+            _ = DataEditor >> DataRelocator;
+            _ = CommandProcessor >> DataEditor >> DataBaseDifferenceHandler;
+            _ = CommandProcessor >> DataRelocator >> DataBaseDifferenceHandler;
+            _ = CommandProcessor >> DataGetter;
         }
 
         public void ApplicationLaunched()
         {
-            UIRouter.RequestNavigation(new NavigationInfo() { Name = UIViews.OVERALL_PAGE });
+            UIRouter.RequestNavigation(new NavigationHistory() { Name = UIViews.OVERALL_PAGE });
         }
     }
 }
