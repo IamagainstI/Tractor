4b1afa22d008d073c4ec5b0ae91c539e8b34e28a
DjArt <art.letter.mail@gmail.com>
2019-12-08 20:27:49 +0700

    В ProjectsCollectionPresenter добавил AddProjects, RemoveProject, OpenProject

--- Tractor.Core/Presenters/Projects/ProjectsCollectionPresenter.cs
+++ Tractor.Core/Presenters/Projects/ProjectsCollectionPresenter.cs
@@ -3,16 +3,52 @@ using System.Collections.Generic;
 using System.Text;
 using Tractor.Core.Collections;
 using Tractor.Core.Objects.Projects;
+using Tractor.Core.Routers.UI;
+using Tractor.Core.Objects.DataBases;
+using Tractor.Core.Routers.Command;
 
 namespace Tractor.Core.Presenters.Projects
 {
-    public class ProjectsCollectionPresenter
+    public class ProjectsCollectionPresenter : AbstractPresentor
     {
         public IProjectStorage Projects { get; }
 
-        public ProjectsCollectionPresenter(IProjectStorage projects)
+        public ProjectsCollectionPresenter(UIRouter router, IProjectStorage projects) : base(router)
         {
             Projects = projects;
         }
+
+        public void AddProject()
+        {
+            NavigationHistory info = new NavigationHistory()
+            {
+                Name = UIViews.TEAMS_MANAGEMENT_PAGE,
+                PresenterType = typeof(ProjectManagementPagePresenter),
+                Paths = new[]
+                {
+                    new List<Guid>(Router.CurrentDataBase.GetPath(Projects)),
+                    new List<Guid>() { Guid.NewGuid() }
+                }
+            };
+            Router.RequestNavigation(info);
+        }
+
+        public void RemoveProject(IProject project)
+        {
+            RelocateCommand cmd = new RelocateCommand(Guid.NewGuid())
+            {
+                DataBase = Router.CurrentDataBase,
+                Entity = Router.CurrentAccount,
+                NewPath = null,
+                Path = new List<Guid>(Router.CurrentDataBase.GetPath(Projects))
+            };
+        }
+
+        public void OpenProject()
+        {
+
+        }
+
+
     }
 }
