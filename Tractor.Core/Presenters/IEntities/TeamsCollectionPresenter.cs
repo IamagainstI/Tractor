3ee3042fc56df329e8469a46a82415162e3b47d7
DjArt <art.letter.mail@gmail.com>
2019-12-08 20:24:17 +0700

    TeamsCollectionPresenter added

---
+++ Tractor.Core/Presenters/IEntities/TeamsCollectionPresenter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects;
+using Tractor.Core.Routers.UI;
+using Tractor.Core.Objects.DataBases;
+
+namespace Tractor.Core.Presenters.IEntities
+{
+    public class TeamsCollectionPresenter : AbstractPresentor
+    {
+        public IEnumerable<ITeam> Teams { get; }
+        UIRouter UIRouter { get; }
+
+        public TeamsCollectionPresenter(UIRouter router, IEnumerable<ITeam> teams) : base(router)
+        {
+            Teams = teams;
+        }
+
+        public void AddTeam()
+        {
+            NavigationHistory info = new NavigationHistory()
+            {
+                Name = UIViews.TEAMS_MANAGEMENT_PAGE,
+                PresenterType = typeof(TeamManagmentPresentor),
+                Paths = new[]
+                {
+                    new List<Guid>(Router.CurrentDataBase.GetPath(UIRouter.CurrentDataBase)),
+                    new List<Guid>() { Guid.NewGuid() }
+                }
+            };
+            Router.RequestNavigation(info);
+        }
+
+        public void RemoveTeam(ITeam team)
+        {
+
+        }
+
+    }
+}
