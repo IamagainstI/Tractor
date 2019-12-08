998820b76f9918262a851b6bb64344fc21b9d6a2
DjArt <art.letter.mail@gmail.com>
2019-12-08 20:23:31 +0700

    EnityManagmentPresenter added

---
+++ Tractor.Core/Presenters/IEntities/EntityManagmentPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tractor.Core.Objects;
+using Tractor.Core.Routers.UI;
+
+namespace Tractor.Core.Presenters.IEntities
+{
+    public class EntityManagmentPresenter : AbstractPresentor
+    {
+        public IEntity Entity { get; }
+
+        public EntityManagmentPresenter(UIRouter router, IEntity entity) : base(router)
+        {
+            Entity = entity;
+        }
+
+    }
+}
