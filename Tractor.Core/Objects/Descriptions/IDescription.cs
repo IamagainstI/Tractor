8167987a19299dcf71fa9d2effc525dd77b8acf6
DjArt <art.letter.mail@gmail.com>
2019-12-09 20:09:36 +0700

    Приведён к соответсвующему виду IDescription

--- Tractor.Core/Objects/Descriptions/IDescription.cs
+++ Tractor.Core/Objects/Descriptions/IDescription.cs
@@ -2,18 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using EmptyBox.IO.Storage;
+using Tractor.Core.Collections;
 using Tractor.Core.Objects.Descriptions.Labels;
 
 namespace Tractor.Core.Objects.Descriptions
 {
     public interface IDescription : IEquatable<IDescription>, INotifyPropertyChanged, INotifyPropertyChanging, ICloneable
     {
-		IList<ILabel> Labels { get; }
+		ObservableCollection<ILabel> Labels { get; }
+        ObservableCollection<IStorageItem> Attachments { get; }
         Guid ID { get; }
-        IList<IStorageItem> Attachments { get; }
-        void AddLabel(IEnumerable<ILabel> label);
-        void RemoveLabel(IEnumerable<ILabel> label);
-        void AddAttachment(IEnumerable<IStorageItem> storageItem);
-        void RemoveAttachment(IEnumerable<IStorageItem> storageItem);
     }
 }
