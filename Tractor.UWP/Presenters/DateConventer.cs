63e3c3fa2316bd7c1c968187225ce4057155d0fc
sivtsev.107911 <sivtsev.107911@gmail.com>
2019-12-10 18:14:42 +0700

    добавлен DateConventer

---
+++ Tractor.UWP/Presenters/DateConventer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tractor.Core.Objects.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Tractor.UWP.Presenters
+{
+    class DateConventer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value switch
+            {               
+                DateTime s => DateTime.Now.ToString(s.Day, s.Month, s.Year),
+                DateTime uwp => uwp,
+                _ => throw new ArgumentException(),
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
