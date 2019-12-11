9bf1d9fafc06c17bd2fe66a35f3ab90fe90cf810
DjArt <art.letter.mail@gmail.com>
2019-12-12 00:33:03 +0700

    Методы для презенторов собраны воедино

---
+++ Tractor.Core/Presenters/TaskMethods.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tractor.Core.Objects.DataBases;
+using Tractor.Core.Objects.Tasks;
+using Tractor.Core.Presenters.Tasks;
+using Tractor.Core.Routers.Command;
+using Tractor.Core.Routers.UI;
+
+namespace Tractor.Core.Presenters
+{
+    public static class TaskMethods
+    {
+        public static void AddTask(UIRouter router, ITaskStorage storage) => EditTask(router, storage, null);
+
+        public static void RemoveTask(UIRouter router, ITask task) => MoveTask(router, task, null);
+
+        public static void MoveTask(UIRouter router, ITask task, ITaskStorage newStorage)
+        {
+            RelocateCommand cmd = new RelocateCommand(Guid.NewGuid())
+            {
+                DataBase = router.CurrentDataBase,
+                Entity = router.CurrentAccount,
+                NewPath = router.CurrentDataBase.GetPath(newStorage),
+                Path = router.CurrentDataBase.GetPath(task.Parent)
+            };
+            router.SendCommand(cmd);
+        }
+
+        public static void SaveTask(UIRouter router, ITaskStorage storage, ITask task)
+        {
+            SetCommand cmd = new SetCommand(Guid.NewGuid())
+            {
+                DataBase = router.CurrentDataBase,
+                Entity = router.CurrentAccount,
+                NewValue = task,
+                Path = router.CurrentDataBase.GetPath(storage).Concat(Enumerable.Repeat(task.ID, 1))
+            };
+            router.SendCommand(cmd);
+        }
+
+        public static void EditTask(UIRouter router, ITaskStorage storage, ITask task)
+        {
+            NavigationHistory navigationHistory = new NavigationHistory()
+            {
+                Name = UIViews.TASK_EDITOR,
+                PresenterType = typeof(TaskEditPresenter),
+                Paths = new IEnumerable<Guid>[]
+                {
+                    router.CurrentDataBase.GetPath(storage),
+                    router.CurrentDataBase.GetPath(task)
+                }
+            };
+            router.RequestNavigation(navigationHistory);
+        }
+
+        public static void ShowTask(UIRouter router, ITask task)
+        {
+            NavigationHistory navigationHistory = new NavigationHistory()
+            {
+                Name = UIViews.TASK_VIEW_DIALOG,
+                PresenterType = typeof(TaskViewPresenter),
+                Paths = new IEnumerable<Guid>[]
+                {
+                    router.CurrentDataBase.GetPath(task)
+                }
+            };
+            router.RequestNavigation(navigationHistory);
+        }
+    }
+}
