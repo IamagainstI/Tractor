1619bb033e7d3516a17b0759187a891623829c0e
DjArt <art.letter.mail@gmail.com>
2019-12-12 00:37:54 +0700

    Селектор отображений на основе типа для ITask

---
+++ Tractor.UWP/Presenters/Tasks/TaskViewTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tractor.Core.Objects.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Tractor.UWP.Decorators.Tasks
+{
+    public sealed class TaskViewTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ITaskTemplate { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            switch (item)
+            {
+                case ITask _:
+                    return ITaskTemplate;
+                default:
+                    return null;
+            }
+        }
+    }
+}
