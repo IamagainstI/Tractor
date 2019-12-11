bb2d6f485b9902641de46954915c30c90ac86cde
DjArt <art.letter.mail@gmail.com>
2019-12-11 20:43:13 +0700

    TaskView переработан

--- Tractor.UWP/Decorators/Controls/TaskView.xaml.cs
+++ Tractor.UWP/Decorators/Controls/TaskView.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Tractor.Core.Model;
 using Tractor.Core.Objects.Tasks;
+using Tractor.Core.Routers.Command;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -23,6 +24,8 @@ namespace Tractor.UWP.Decorators.Controls
     {
         public static readonly DependencyProperty PresentedTaskProperty = DependencyProperty.Register(nameof(PresentedTask), typeof(ITask), typeof(TaskView), new PropertyMetadata(null));
 
+        public event EventHandler<ICommand> CommandRequested;
+
         public ITask PresentedTask
         {
             get => (ITask)GetValue(PresentedTaskProperty);
@@ -35,6 +38,23 @@ namespace Tractor.UWP.Decorators.Controls
         }
 
         private void b_AddDescription_Click(object sender, RoutedEventArgs e)
+        {
+            CommandRequested?.Invoke(this, null);
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width >= 512)
+            {
+                VisualStateManager.GoToState(this, "WideView", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "NarrowView", true);
+            }
+        }
+
+        private void tb_Name_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
         {
 
         }
