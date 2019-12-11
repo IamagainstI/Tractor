abbf743bca2da953bd647c45c56cf85feafcdaab
DjArt <art.letter.mail@gmail.com>
2019-12-12 01:37:48 +0700

    Добавлен ProjectEditPresenter

---
+++ Tractor.Core/Presenters/Projects/ProjectEditPresenter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Tractor.Core.Objects.DataBases;
+using Tractor.Core.Objects.Projects;
+using Tractor.Core.Objects.Tasks;
+using Tractor.Core.Presenters.Tasks;
+using Tractor.Core.Routers.Command;
+using Tractor.Core.Routers.UI;
+
+namespace Tractor.Core.Presenters.Projects
+{
+    public class ProjectEditPresenter : AbstractPresenter
+    {
+        public IProject Project { get; set; }
+        public IProjectStorage Storage { get; }
+
+        public ProjectEditPresenter(UIRouter router, IProjectStorage storage, IProject project) : base(router)
+        {
+            Project = project;
+            Storage = storage;
+        }
+
+        public void Save() => ProjectMethods.SaveProject(Router, Storage, Project);
+        public void Cancel() => Router.RequestBack();
+    }
+}
